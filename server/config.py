62514c237a10b75737bde24fe3fc03fc44629f2b
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-24 22:27:43 -0400

    adding changes in config

--- server/config.py
+++ server/config.py
@@ -1,17 +1,21 @@
-from flask import flask
-from flask_brypt import Bcrypt
+from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 app=Flask(__name__)
-app.secret_key = 'YOUR_APP_SECRET_HERE'  # in terminal run, python -c 'import os; print(os.urandom(16))'
+app.secret_key = b'\xec\xe3\r\xf3\x97\x07\x19ss\xb6\xea\xe4M%S\xd9'  # in terminal run, python -c 'import os; print(os.urandom(16))'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-db = SQLAlchemy()
-migrate = Migrate(app,db)
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
