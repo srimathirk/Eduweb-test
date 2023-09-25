2c713fab0bc9a4bfb2df9099693df30ba69248c9
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-24 23:06:29 -0400

    adding changes to py

--- server/app.py
+++ server/app.py
@@ -5,6 +5,16 @@ from config import app, db, api # this line will run config.py and initialise ou
 from models import User
 
 #All routes herre
+@app.route("/", methods=["GET"])
+def root():
+    return "<h1>Welcome to educa Web Testing App!</h1>"
+
+#RESTful route syntax
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()] #Serialize users - password hashes shouldnot be sent to client  
+        return users, 200
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
