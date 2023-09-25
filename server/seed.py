004f20d970c4152b075a13c03338f26834bb1b3b
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-24 22:27:59 -0400

    adding changes to seed

--- server/seed.py
+++ server/seed.py
@@ -1,4 +1,4 @@
-from faker import faker
+from faker import Faker
 from models import User
 from config import db, app, bcrypt
 
@@ -6,8 +6,10 @@ faker = Faker()
 
 with app.app_context():
 
+    print('Deleting User..')
     User.query.delete()
-    print(Deleting User..)
+    print('Creating User..')
+    
 
     for _ in range(10):
         username = faker.profile(fields=['username'])['username']
