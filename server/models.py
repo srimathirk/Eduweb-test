96ea32e1881248ba4916b210c19bfa14a74f0ea3
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-24 22:14:10 -0400

    creating models

---
+++ server/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property #Restrict access to password hash
+    def password_hash(self):
+        raise Exception("Password hashes may not be viewed.")
+
+    @password_hash.setter #Generate a Bcrypt password hash and set it to _password_hash attribute
+    def password_hash(self, password):
+        bcrypt_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self._password_hash = bcrypt_hash
+
+    def authenticate(self, password): #check if provided password matches one that stored in db
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
