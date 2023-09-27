402a3f60ef7f0dc40737a1c82f73485a19aa3f71
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-27 14:29:55 -0400

    adding reviews, ratings, Books, user-books tables

--- server/models.py
+++ server/models.py
@@ -3,12 +3,20 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
+# Define association table for many-to-many relationship between users and books
+user_books = db.Table('user_books',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True)
+)
+
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     _password_hash = db.Column(db.String)
+    
+    books = db.relationship('Book', secondary= user_books)
 
     @hybrid_property #Restrict access to password hash
     def password_hash(self):
@@ -24,3 +32,28 @@ class User(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"User {self.username}, ID: {self.id}"
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Author = db.Column(db.String)
+    Title = db.Column(db.String, nullable=False)
+    Image = db.Column(db.String)
+    pdf =  db.Column(db.String)
+    users = db.relationship('User', secondary=user_books, overlaps="books")
+    reviews = db.relationship('Review', backref='books')
+    ratings = db.relationship('Rating', backref='books')
+  
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
